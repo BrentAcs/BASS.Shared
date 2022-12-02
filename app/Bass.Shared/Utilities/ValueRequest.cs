13b3d97768ac946446b69b1f53e958ed42ba3365
Brent Acs <brent.mann@gmail.com>
2022-12-01 19:51:02 -0500

    added ValuesRequest object.

--- app/Bass.Shared/Utilities/ValueRequest.cs
+++ app/Bass.Shared/Utilities/ValueRequest.cs
@@ -47,31 +47,6 @@ public class ValueRequest<T>
       
       throw new NotImplementedException();
    }
-
-   public IEnumerable<T> GetValues(IRng rng, int uniqueCount)
-   {
-      if (_subsetSelection is null)
-         throw new InvalidOperationException("GetValues called w/o a subset selection.");
-      if (uniqueCount < 0)
-         throw new ArgumentOutOfRangeException(nameof(uniqueCount), "Must be positive.");
-      if (uniqueCount > _subsetSelection.Count())
-         throw new ArgumentOutOfRangeException(nameof(uniqueCount), "Must be less than or equal to subset selection count.");
-      
-      var values = new List<T>();
-
-      for (int i = 0; i < uniqueCount; i++)
-      {
-         T value = rng.Next(_subsetSelection);
-         while (values.Contains(value))
-         {
-            value = rng.Next(_subsetSelection);
-         }
-         
-         values.Add(value);         
-      }
-
-      return values;
-   }
    
    private void SetAbsoluteValue(T? value)
    {
