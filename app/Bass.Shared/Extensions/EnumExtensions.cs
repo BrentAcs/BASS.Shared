84076d11feac748339ad705c874d9dcd06e6dc4e
Brent Acs <brent.mann@gmail.com>
2023-11-29 22:13:20 -0500

    Features/mongo extensions (#11)
    * Added some mongo support.
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP

---
+++ app/Bass.Shared/Extensions/EnumExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Bass.Shared.Utilities;
+
+namespace Bass.Shared.Extensions;
+
+public static class EnumExtensions
+{
+   public static T? GetAttributeOfType<T>(this Enum enumVal) where T : Attribute
+   {
+      var type = enumVal.GetType();
+      var memInfo = type.GetMember(enumVal.ToString());
+      var attributes = memInfo[0].GetCustomAttributes(typeof(T), false);
+      return (attributes.Length > 0) ? (T)attributes[0] : null;
+   }
+
+   public static T GetRandomValue<T>(this IRng rng) //where T : Enum
+   {
+      Enum.GetValues(typeof(T));
+
+      throw new NotImplementedException();
+   }
+}
+
+public enum Test
+{
+   A,
+   B
+}
---
+++ app/Bass.Shared/Extensions/NewtonsoftJsonExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace Bass.Shared.Extensions;
+
+public static class NewtonsoftJsonExtensions
+{
+   public static string? ToJson(this object? obj, JsonSerializerSettings? settings = null)
+   {
+      if (obj == null)
+         return null;
+
+      settings ??= new JsonSerializerSettings();
+
+      return JsonConvert.SerializeObject(obj, settings);
+   }
+
+   public static void ToJsonFile(this object obj, string filename, JsonSerializerSettings? settings = null)
+   {
+      settings ??= new JsonSerializerSettings
+      {
+         DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate,
+         Formatting = Formatting.Indented
+      };
+
+      File.WriteAllText(filename, obj.ToJson(settings));
+   }
+
+   public static T? FromJson<T>(this string json, JsonSerializerSettings? settings = null)
+   {
+      settings ??= new JsonSerializerSettings();
+      settings.TypeNameHandling = TypeNameHandling.All;
+      return JsonConvert.DeserializeObject<T>(json, settings);
+   }
+
+   public static T? FromJsonFile<T>(this string filename, JsonSerializerSettings? settings = null)
+   {
+      settings ??= new JsonSerializerSettings { DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate };
+      return File.ReadAllText(filename).FromJson<T>(settings);
+   }
+}
