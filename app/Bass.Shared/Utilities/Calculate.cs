524c6f221504bbe358e539b61f8be1457da82104
Brent Acs <brent.mann@gmail.com>
2024-06-07 20:22:41 -0400

    adding Calculate file

---
+++ app/Bass.Shared/Utilities/Calculate.cs
@@ -0,0 +1,12 @@
+﻿using Bass.Shared.Geometry;
+
+namespace Bass.Shared.Utilities;
+
+public static class Calculate
+{
+   public static double Distance(this Point3dF pointFrom, Point3dF pointTo) =>
+      Distance(pointFrom.X, pointFrom.Y, pointFrom.Z, pointTo.X, pointTo.Y, pointTo.Z);
+
+   public static double Distance(double x1, double y1, double z1, double x2, double y2, double z2) =>
+      Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+}
